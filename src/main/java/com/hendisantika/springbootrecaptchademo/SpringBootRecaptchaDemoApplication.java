47987b99d2f4ce48cbd700b4c4dab75c185c0b02
Hendi Santika <hendisantika@gmail.com>
2017-12-05 06:30:59 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootrecaptchademo/SpringBootRecaptchaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootrecaptchademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecaptchaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecaptchaDemoApplication.class, args);
+	}
+}
